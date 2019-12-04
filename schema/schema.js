56f768ec587bcf5bbf4c0bbdbb74b54341207176
Vinni Hoke <vinni.hoke@gmail.com>
2019-12-03 22:19:46 -0800

    Tutorial 8

--- schema/schema.js
+++ schema/schema.js
@@ -3,8 +3,9 @@ const graphql = require('graphql');
 // This will describe the data on the graph, and how to interact with it.
 
 // I couldn't load in 'as' something else.
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
 
+// Creating an object type.
 const BookType = new GraphQLObjectType({
 	name: 'Book',
 	// He specifically used the ES6 function.
@@ -14,3 +15,31 @@ const BookType = new GraphQLObjectType({
 		genre: { type: GraphQLString },
 	})
 })
+
+// We now need to add the root queries.
+
+// This is the Query from the front end.
+const RootQuery = new GraphQLObjectType({
+	name: 'RootQueryType',
+	fields: {
+		book: {
+			type: BookType,
+			// We're expecting that they'll pass in this id when they are looking for a specific book.
+
+			// book(id:'123'){
+				// 	name
+				// 	genre
+				// }
+			args: {
+				id: { type: GraphQLString }
+			},
+			resolve(parent, args){
+				// Code to get data from db / other source
+				// Parent comes into play when we look at relationships. We now need to tell graphQL what to do when someone makes this request.
+			}
+		}
+	}
+});
+
+// Exporting the new schema and telling it which query to use.
+module.exports = new GraphQLSchema({ query: RootQuery })
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const graphqlHTTP = require('express-graphql');
+const schema = require('../schema/schema.js');
 
 
 const server = express();
@@ -11,7 +12,7 @@ server.use(morgan('dev'));
 server.use('/graphql', graphqlHTTP({
 	// You must pass in a schema on how our graph will look. What datatypes are on the graph, how are they structured, and how are they related? 
 	// We'll be pulling from the schema.js file inside schema folder.
-
+	schema
 }))
 
 module.exports = server;
