e50f36a1f3547b76d5a188309d6503540100d290
Vinni Hoke <vinni.hoke@gmail.com>
2019-12-03 23:32:01 -0800

    Tutorial 9

--- schema/schema.js
+++ schema/schema.js
@@ -1,9 +1,16 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 // This will describe the data on the graph, and how to interact with it.
 
 // I couldn't load in 'as' something else.
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+var books = [
+	{ name: "Book1", genre: "Fantasy", id: '1' },
+	{ name: "Book2", genre: "Fiction", id: '2' },
+	{ name: "Book3", genre: "Non-fiction", id: '3' }
+]
 
 // Creating an object type.
 const BookType = new GraphQLObjectType({
@@ -35,7 +42,10 @@ const RootQuery = new GraphQLObjectType({
 			},
 			resolve(parent, args){
 				// Code to get data from db / other source
-				// Parent comes into play when we look at relationships. We now need to tell graphQL what to do when someone makes this request.
+				// Parent comes into play when we look at relationships. We now need to tell graphQL what to do when someone makes this request. Doesn't matter where this data is stored, but this is where we would pull it... i.e. MongoDB or sqlite3.
+				
+				// Here we're using lodash to check for any book with an id of args.id
+				_.find(books, { id: args.id });
 			}
 		}
 	}
