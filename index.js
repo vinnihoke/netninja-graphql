1dddbde9d183103b0fef2d067b2e18e7ce11a5c5
Vinni Hoke <vinni.hoke@gmail.com>
2019-12-04 22:54:16 -0800

    Added mongoDB

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 require('dotenv').config()
 const server = require('./server/server.js');
+const mongoose = require('mongoose')
 
 const PORT = process.env.PORT || 5000
+
+const { MONGO_PASSWORD, MONGO_USER } = process.env
+
+mongoose.connect(`mongodb+srv://${MONGO_USER}:${MONGO_PASSWORD}@graphql-netninja-tutorial-ttzjs.gcp.mongodb.net/test?retryWrites=true&w=majority`)
+mongoose.connection.once('open', () => {
+	console.log("Connected to MongoDB");
+})
 
 server.listen(PORT, () => {
 	console.log(`::: Listening on http://localhost:${PORT} :::`);
